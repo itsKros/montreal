a11e611560be3e31f9235ae022fa4a8e9dadb704
itsKros <kroshan197@gmail.com>
2019-10-22 18:48:37 +0530

    reset password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -14,7 +14,7 @@
     </div>
 </section>
 @section('content')
-<div class="container">
+<div class="container" style="padding:60px 0 80px">
     <div class="row justify-content-center">
 
             <div class="col-md-6 col-xs-12">
@@ -43,55 +43,10 @@
                                     </button>
                             </p>
                         </form>
-                        <div class="tb-info-login ">
-                            <h5 class="tb-title4">SignUp today and you'll be able to:</h5>
-                            <ul>
-                                <li>Speed your way through the checkout.</li>
-                                <li>Track your orders easily.</li>
-                                <li>Keep a record of all your purchases.</li>
-                            </ul>
-                        </div>
+                        
                     </div>
                 </div>
-        <div class="col-md-8">
-            <div class="panel panel-default">
-                <div class="panel-heading"><h3 class="panel-title">{{ __('Reset Password') }}</h3></div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 @endsection
